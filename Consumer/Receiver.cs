dce954774d2b827a3f2fb53bbd430c2c75b78286
Oleksii Maievskyi <oleksii.maievskyi@dxc.com>
2023-03-08 17:46:51 +0200

    Add project files.

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,17 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("TestMessage", false, false, false, null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) => { var body = ea.Body.Span; string message = Encoding.UTF8.GetString(body); Console.WriteLine($"Received message: {message}..."); };
+
+channel.BasicConsume("TestMessage", true, consumer);
+
+Console.WriteLine("Press enter to exit consumer...");
+Console.ReadLine();
---
+++ Producer/Sender.cs
@@ -0,0 +1,17 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName= "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("TestMessage", false, false, false, null);
+
+string message = "This message will be posted to RabbitMQ server via producer!!!";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(String.Empty, "TestMessage", null, body);
+Console.WriteLine($"Sent message {message}...");
+
+Console.WriteLine("Press enter to exit sender...");
+Console.ReadLine();
